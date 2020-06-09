c101489ab51d94305c38706668ee3f1a03654981
João Guilherme <j4g3dev@gmail.com>
2020-06-09 20:52:54 -0300

    Add: option pixel or value

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ import { Platform, StatusBar, Dimensions } from "react-native";
 const { height, width } = Dimensions.get("window");
 const standardLength = width > height ? width : height;
 const offset =
-  width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight; // iPhone X style SafeAreaView size in portrait
+  width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight;
 
 const deviceHeight =
   isIphoneX() || Platform.OS === "android"
     ? standardLength - offset
     : standardLength;
 
-export function RFPercentage(percent) {
-  const heightPercent = (percent * deviceHeight) / 100;
-  return Math.round(heightPercent);
+export function RFPercentage(percent, type) {
+  if (type === "pixel") {
+    const heightPercent = (percent * deviceHeight) / 100;
+    return `${Math.round(heightPercent)}px`;
+  } else {
+    const heightPercent = (percent * deviceHeight) / 100;
+    return Math.round(heightPercent);
+  }
 }
 
-// guideline height for standard 5" device screen is 680
 export function RFValue(fontSize, standardScreenHeight = 680) {
   const heightPercent = (fontSize * deviceHeight) / standardScreenHeight;
   return Math.round(heightPercent);
